8d7793f015735651dcecd6cef8479e4f9cef3a71
Emi004 <aenoaeiemi49@gmail.com>
2025-03-19 16:16:52 +0200

    some code

--- alu.v
+++ alu.v
@@ -1,4 +1,16 @@
+`include "bitadder8.v"
+`include "bitmultiplier.v"
+`include "bitdivision.v"
+`include "mux4.v"
+
 module alu (
 );
-    
+    wire[8:0]a,b;    
+
+    bitadder8 BITADDER8();
+    bitadder8 BITSUBTRACTOR8();
+    bitmultiplier BITMULTIPLIER();
+    bitdivision BITDIVISION();
+    mux4 MUX4();
+
 endmodule
